02cfb24010e91bf201ca0fb74a309536a4658a17
DESKTOP-0NDTVM7\Tuan <vantuankrn197@gmail.com>
2018-09-12 12:09:30 +0700

    update

--- book/resources/views/layouts/app.blade.php
+++ book/resources/views/layouts/app.blade.php
@@ -67,7 +67,7 @@
                                         </a>
 
                                         <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
+                                                        
                                         </form>
                                     </li>
                                 </ul>
